97a761dad3a82bce8d1395c107d94d899c758aff
Ahmed Elmalki <elmalkiahmed02@gmail.com>
2024-05-02 13:55:02 +0100

    3la lah

--- main/main.go
+++ main/main.go
@@ -9,34 +9,43 @@ import (
 )
 
 func main() {
-	if len(os.Args) == 3 {
-		fileName := os.Args[1]
-		file, err := os.ReadFile(fileName)
-		if err != nil {
-			fmt.Printf("Error")
-			return
-		}
-		input := strings.Split(string(file), "\n")
-		var line []string
-		output := ""
-		for i := 0; i < len(input); i++ {
-			line = strings.Split(input[i], " ")
-			line = goreloaded.UpLowandCap(line)
-			line = goreloaded.HexandBin(line)
-			line = goreloaded.Formspaces(line)
-			line = goreloaded.AtoAN(line)
-			temp := strings.Join(line, " ")
-			temp = goreloaded.FixPunc(temp)
-			temp = goreloaded.SingleQuote(temp)
-			temp = strings.Trim(temp, " ")
-			output += temp + "\n"
-		}
-		err = os.WriteFile(os.Args[2], []byte(output), 0o644)
-		if err != nil {
-			fmt.Printf("Error")
-			return
-		}
-	} else if len(os.Args) != 3 {
-		fmt.Println("error: u did not use 3 args")
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: ./[program_name] [input_file] [name_of_output]")
 	}
+
+	fileName := os.Args[1]
+	file, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Printf("Error")
+		return
+	}
+	input := strings.Split(string(file), "\n")
+	output := processInput(input)
+	err = os.WriteFile(os.Args[2], []byte(output), 0o644)
+	if err != nil {
+		fmt.Printf("Error")
+		return
+	}
+}
+
+func formatText(input string) string {
+	var line []string
+	line = strings.Split(input, " ")
+	line = goreloaded.UpLowandCap(line)
+	line = goreloaded.HexandBin(line)
+	line = goreloaded.Formspaces(line)
+	line = goreloaded.AtoAN(line)
+	tmp := strings.Join(line, " ")
+	tmp = goreloaded.FixPunc(tmp)
+	tmp = goreloaded.SingleQuote(tmp)
+	tmp = strings.Trim(tmp, " ")
+	return (tmp + "\n")
+}
+
+func processInput(input []string) string {
+	output := ""
+	for i := 0; i < len(input); i++ {
+		output += formatText(input[i])
+	}
+	return output
 }
